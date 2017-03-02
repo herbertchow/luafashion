2a0010fc2eea2c6fcf6dde1dd0a24685c43a18ba
netease_wb1 <495567615@qq.com>
2017-03-02 18:51:58 +0800

    第二次提交

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 		</div>
 	</div>
 	<div class="hidden-xs">
-		<div class="container margin-15">	
+		<div class="container margin-15">	 
 			<div class="row">
 				<div class="col-xs-12" >
 					<img class="logo-big" src="images/lua2.png" alt="">
